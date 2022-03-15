f20584edff0b1415e96c03f7190ea83c2f532b4f
Bad Boy <sonupanda078@gmail.com>
2022-03-15 22:24:35 +0530

    ,

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,19 @@
-console
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+      try {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://badboy.is-a.dev/receive/message", true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+          message: e.target.elements.message.value,
+          name: e.target.elements.name.value,
+          email: e.target.elements.email.value
+        }));
+        alert("Message sent!");
+      } catch(err) {
+        alert("An Error occured:\n"+err);
+      }
+      e.preventDefault();
+      e.target.elements.name.value = '';
+      e.target.elements.email.value = '';
+      e.target.elements.message.value = '';
+    });

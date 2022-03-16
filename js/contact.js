c1a4783471e20e1e8721001e5d23e4141cf173e1
Bad Boy <sonupanda078@gmail.com>
2022-03-16 18:38:52 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,27 +1,24 @@
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-      try {
-        var xhr = new window.XMLHttpRequest();
-        xhr.open("POST", "https://badboy.is-a.dev/receiver", true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify({
-          type: "message",
-          message: e.target.elements.message.value,
-          name: e.target.elements.name.value,
-          email: e.target.elements.email.value
-        }));
-        xhr.onreadystatechange = function() {
-          if (xhr.readyState == 4 && xhr.status == 200) {
-            alert("Message sent!");
-            console.log(xhr.response)
-          } else {
-            alert("Couldnt deliver the message.")
-          }
-        }
-      } catch(err) {
-        alert("An Error occured:\n"+err);
-      }
-      e.preventDefault();
-      e.target.elements.name.value = '';
-      e.target.elements.email.value = '';
-      e.target.elements.message.value = '';
-    });
+document.querySelector('#contact-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    try {
+        let resp = await fetch("https://badboy.is-a.dev/receiver", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                type: "message",
+                message: e.target.elements.message.value,
+                name: e.target.elements.name.value,
+                email: e.target.elements.email.value
+            })
+        });
+        alert("Message sent!");
+        console.log(resp)
+    } catch (err) {
+        alert("An Error occured:\n" + err);
+    }
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+});

efdafb1f19ddb100f18e4f9fd208d792215217be
Bad Boy <sonupanda078@gmail.com>
2022-03-16 11:11:46 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,22 @@
 document.querySelector('#contact-form').addEventListener('submit', (e) => {
       try {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "https://badboy.is-a.dev/receive/message", true);
+        var xhr = new window.XMLHttpRequest();
+        xhr.open("POST", "https://badboy.is-a.dev/receiver", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({
+          type: "message",
           message: e.target.elements.message.value,
           name: e.target.elements.name.value,
           email: e.target.elements.email.value
         }));
-        alert("Message sent!");
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            alert("Message sent!");
+            console.log(xhr.response)
+          } else {
+            alert("Couldnt deliver the message.")
+          }
+        }
       } catch(err) {
         alert("An Error occured:\n"+err);
       }
